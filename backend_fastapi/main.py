297138ce5c93ddf249904603f0b2177303438109
mizuki37zuki <2310001@i-seifu.jp>
2025-11-25 10:59:36 +0900

    とりあえず、通知バナーから特定のページを開くようにできた

--- backend_fastapi/main.py
+++ backend_fastapi/main.py
@@ -26,31 +26,15 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/send_push/")
-def send_push(token: str):
-    # registration_token = "f05Mjz9Q733uxPKhPw6spC:APA91bG8U0JzQ-e8i5xiZ99MEQCDDIhBcn0gsEAr40-xh97XiZuK9cIZWzlpfaVECh6L5M24isaFOCMadUHIBJB3CCN3GwelatGEpkqKJN8zenLA2QBTVds"
-
+def send_push(token: str, path: str):
     message = messaging.Message(
-        notification=messaging.Notification(
-            title="Pythonからのメッセージ",
-            body="これはテストです。",
-        ),
-        # webpush=messaging.WebpushConfig(
-        #     fcm_options=messaging.WebpushFCMOptions(
-        #         link=FRONT_URL  # HTTPSじゃないとだめみたい
-        #     )
-        # ),
+        data={
+            "title": "Pythonからのメッセージ！！！",
+            "body": "ここが本文です",
+            "path": path,
+        },
         token=token,
     )
-
-    # message = messaging.Message(
-    #     data={
-    #         "title": "Pythonからのメッセージ",
-    #         "body": "これはテストです。",
-    #         "url": "aaa",
-    #         "target_id": "23"
-    #     },
-    #     token=registration_token,
-    # )
 
     # メッセージの送信
     response = messaging.send(message)
--- frontend_react/public/firebase-messaging-sw.js
+++ frontend_react/public/firebase-messaging-sw.js
@@ -11,7 +11,6 @@ firebase.initializeApp({
   measurementId: 'BIZfUZex4jypuLNrqDD6CdglfWjcyCSUkudGoJ8Vn0eoSisl9I02p1fz90H6EO7jXzyj8CzvAEBKSC1ccp3fCNk',
 });
 
-
 const messaging = firebase.messaging();
 
 // バックグラウンド通知（閉じている、または別タブにいる）のときに実行
@@ -20,51 +19,23 @@ messaging.onBackgroundMessage((payload) => {
     '[firebase-messaging-sw.js] バックグラウンドで通知を受け取りました',
     payload
   );
-  
-  // const notificationTitle = payload.notification.title;
-  // const notificationOptions = {
-  //   body: payload.notification.body,
-  //   icon: payload.notification.image
-  // };
 
-  // ブラウザの通知バナー（ネイティブ通知）を表示
-  // self.registration.showNotification(notificationTitle, notificationOptions);
+  // 通知バナーを表示
+  self.registration.showNotification(payload.data.title, {
+    body: payload.data.body,
+    data: {
+      path: payload.data.path
+    }
+  });
 });
 
-// self.addEventListener("push", (event) => {
-//   // const data = event.data.json();
-//   let data = {}
+// 通知をクリックしたら、ウィンドウを開くかフォーカスする
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
 
-//   try {
-//     data = JSON.parse(event.data.text());
-//   } catch (error) {
-//     console.error("Push data parse error:", error)
-//   }
+  const target = event.notification?.data?.path || "/"
 
-//   event.waitUntil(
-//     self.registration.showNotification(data.title, {
-//       body: data.body,
-//       data: { url: data.url, targetId: data.target_id } // データを保持
-//     })
-//   );
-// });
-
-// self.addEventListener("notificationclick", (event) => {
-//   event.notification.close();
-
-//   event.waitUntil(
-//     clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
-//       // 既存タブがあればそこを開く
-//       for (const client of clientList) {
-//         if (client.url.includes("/") && "focus" in client) {
-//           client.postMessage(event.notification.data); // データ送信
-//           return client.focus();
-//         }
-//       }
-//       // タブがなければ新規作成
-//       if (clients.openWindow) {
-//         return clients.openWindow(event.notification.data.url);
-//       }
-//     })
-//   );
-// });
+  event.waitUntil(
+    clients.openWindow(`https://cosfulu.com${target}`)
+  );
+});
